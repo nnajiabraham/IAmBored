2ff38bc18f07d41d4ce8845205e81e10ee88006e
Abraham Nnaji <abraham.nnaji@skipthedishes.ca>
2020-01-13 16:35:03 -0600

    added tweet logic

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net/url"
+	// "os"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	consumerKey       = ""
+	consumerSecret    = ""
+	accessToken       = ""
+	accessTokenSecret = ""
+)
+
+// func getenv(name string) string {
+// 	v := os.Getenv(name)
+// 	if v == "" {
+// 		panic("missing required environment variable " + name)
+// 	}
+// 	return v
+// }
+
+func main() {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	log := &logger{logrus.New()}
+	api.SetLogger(log)
+
+	stream := api.PublicStreamFilter(url.Values{
+		"track": []string{
+			"@IAmBoredBot I am bored", 
+			"@IAmBoredBot suggestion", 
+			"@IAmBoredBot what should I do", 
+			},
+	})
+
+	// api.GetMentionsTimeline
+
+	defer stream.Stop()
+
+	for v := range stream.C {
+		t, ok := v.(anaconda.Tweet)
+		if !ok {
+			log.Warningf("received unexpected value of type %T and not a type of anaconda.Tweet", v)
+			continue
+		}
+
+		if t.RetweetedStatus != nil {
+			continue
+		}
+
+		_, err := api.Retweet(t.Id, false)
+
+		if err != nil {
+			log.Errorf("could not retweet %d: %v", t.Id, err)
+			continue
+		}
+		log.Infof("retweeted %d", t.Id)
+	}
+}
+
+type logger struct {
+	*logrus.Logger
+}
+
+func (log *logger) Critical(args ...interface{})                 { log.Error(args...) }
+func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
+func (log *logger) Notice(args ...interface{})                   { log.Info(args...) }
+func (log *logger) Noticef(format string, args ...interface{})   { log.Infof(format, args...) }
